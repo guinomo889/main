cf1eb180355d2668cd8a11dd6464dc817230166b
guinomo889 <163137192+guinomo889@users.noreply.github.com>
2024-03-12 09:48:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://media-assets.vanityfair.it/photos/653f7b8258781fdcdd15c32d/16:9/w_2560%2Cc_limit/1677496834";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://ew.com/thmb/UyTpPE3AkuPOPZZf6Mf3IL-de_U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/taylor-swift-grammys-2024-tout-1-7be23be37c47435a9861249e62497331.jpg";
+}
